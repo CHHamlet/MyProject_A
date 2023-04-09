f141486ed7ced0ff15f52c96d6fea4f30a363a01
lchenglong22.@gmail.com <lchenglong22.@gmail.com>
2023-04-09 15:44:54 +0800

    post remart

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+//read me
+//RUN hello.js in TERMINAL 
+//RUN [launch.json]'s["url"] word in bowser 
+
 const http = require("http");
 const fs = require("fs");
+const qs = require("querystring");
 
 const Port = 1025;
 const Ip = "127.0.0.1";
@@ -7,10 +12,12 @@ const Ip = "127.0.0.1";
 const sendResponse = (filename,statusCode,response) => {
     fs.readFile(`./html/${filename}.html`,(error,data)=>{
         if (error) {
+            console.log("1");
             response.statusCode = 500;
             response.setHeader("Content-Type", "text/plain");
             response.end("Sorry internal error");
         } else {
+            console.log("2");
             response.statusCode = statusCode;
             response.setHeader("Content-Type", "text/html");
             response.end(data);
@@ -36,20 +43,47 @@ const server = http.createServer((request,response)=>{
             selector="";
         }else if (lang === "zh"){
             selector="-zh";
-    } else {
-      selector = "";
+        } else {
+        selector = "";
         }
 
         if(url==="/"){
             sendResponse(`index${selector}`,200,response);
         } else if (url==="/about"){
             sendResponse(`about${selector}`,200,response);
+        } else if (url==="/login"){
+            sendResponse(`login${selector}`,200,response);
+        } else if (url==="/LoginSuccess"){
+            sendResponse(`LoginSuccess${selector}`,200,response);
+        } else if (url==="/LoginFail"){
+            sendResponse(`LoginFail${selector}`,200,response);
+            console.log("3");
         } else {
             sendResponse(`404${selector}`,404,response);
+            console.log("4");
         }
-    } 
-    else {
+    } else {
+        if (url==="/process-longin"){
+            let body = [];
 
+            request.on("data",(chunk)=>{
+                body.push(chunk);
+            });
+            request.on("end",(chunk)=>{
+                body=Buffer.concat(body).toString();
+                body = qs.parse(body);
+                console.log(body);
+
+                if(body.username ==="li" && body.password === "chenglong"){
+                    response.statusCode = 301;
+                    response.setHeader("Location","/LoginSuccess");
+                }else{
+                    response.statusCode = 301;
+                    response.setHeader("Location","/LoginFail");
+                }
+                response.end();
+            });
+        }
     };
 });
 
